43367104834dca3447f24dec9a27465a6f6b8667
Patrick Canny <patrickcanny@ku.edu>
2017-11-04 11:14:47 -0500

    adding some stuff

--- needle/needle/app.py
+++ needle/needle/app.py
@@ -1,10 +1,26 @@
 import random
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
-from wtforms import Form, StringField, validators
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.debug = True
 
-@app.route("/"):
-def login:
-    return render_template("views/login.html")
+############################
+#                   Functions
+############################
+def authorizeUser():
+    #Spotify Authentication Stuff Goes Here
+
+############################
+#                   Routes
+############################
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
+if __name__ == "__main__":
+    app.run()
