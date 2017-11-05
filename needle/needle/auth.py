b70b2758a119de9c6c5548fe670886737bd1b8b6
zkulphong <zkulphong@gmail.com>
2017-11-05 00:55:20 -0500

    songs function

--- needle/needle/auth.py
+++ needle/needle/auth.py
@@ -49,19 +49,6 @@ class songs(object):
         data = getattr(self, source)
         return data
 
-    def songs_in_list(self, username):
-        songz = []
-        tag = spotify.user_playlists(username, limit = 50, offset = 0)[u'items'][0][u'uri']
-        playlist_id = tag[-22:]
-        _songs = spotify.user_playlist(username, playlist_id)
-        if len(songs[u'tracks'][u'items']) > 0:
-            count = 0
-            while count < len(songs[u'tracks'][u'items']):
-                songz.append(songs[u'tracks'][u'items'][count][u'track'][u'name'])
-                print songs[u'tracks'][u'items'][count][u'track'][u'name']
-                count = count + 1
-
-
 
     def get_playlist_id(self, count, username):
         tag = spotify.user_playlists(username, limit = 50, offset = 0)[u'items'][count][u'uri']
