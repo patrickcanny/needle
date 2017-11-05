b8f985c35dc3ce974b90b3781edd96e9dc61b2af
Milan <milan@ksu.edu>
2017-11-05 01:35:47 -0500

    fixed everything that got deleted

--- needle/needle/app.py
+++ needle/needle/app.py
@@ -1,21 +1,21 @@
 import random
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
-# from auth import playlist, songs, song_info
+from auth import playlist, songs, song_info
 from wtforms import Form, StringField, validators
-# from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.debug = True
 
 #CONFIG MySQL
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'NEWPASSWORD'
-# app.config['MYSQL_DB'] = 'needle'
-# app.config['MYSQL_CURSOR'] = 'DictCursor'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'NEWPASSWORD'
+app.config['MYSQL_DB'] = 'needle'
+app.config['MYSQL_CURSOR'] = 'DictCursor'
 
-# mysql = MySQL(app)
+mysql = MySQL(app)
 
 # ############################
 # #                   Classes
